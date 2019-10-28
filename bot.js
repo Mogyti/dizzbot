06781c21171955c4bc390b8eac72373210845b1d
Hùng Lê <mrlun311@gmail.com>
2019-10-28 14:01:23 +0700

    Ey commit test

--- bot.js
+++ bot.js
@@ -24,13 +24,23 @@ client.on('message', (message) => {
             arrSend = ["Cà khịa 1 tí thì vui, cà khịa nhiều quá ăn dùi cui vào mồm đấy ", "Nhỏ cà chớn, lớn lên đi cà khịa à? "]
         } else if (message.content.toLowerCase().includes("láo")) {
             arrSend = ["Cuộc sống giống như điệu nhảy Tango, tao lùi 1 bước thì mày chỉ được tiến 1 bước thôi, đừng có mà nhờn "]
-        } else if (message.content.toLowerCase().includes("?")) {
+        } else if (message.content.toLowerCase().includes("?") && !message.content.toLowerCase().includes("ey")) {
             arrSend = ["Toàn hỏi thừa thôi", "Mày nói xem lý do gì tao phải trả lời câu hỏi của mày? ", "Hỏi cái gì? Thích ăn đòn không? "]
         } else if ((message.content.toLowerCase().includes("giao") || message.content.toLowerCase().includes("thịt")) && message.author.id === "224258714037780480") {
             arrSend = ["Anh cứ để đó cho em, riêng quả này em chấp 1 mắt <:pepega:636040268893650963>", "Nó hành anh mất cân, em trả lại nó ngần đấy tấn <:giotaylen:535489803815223297>",
                         "Thôi ông tự đi mà giải quyết đi, nhà tôi bao việc <:khinh:535335174427115521>"]
         } else if (message.content.toLowerCase().includes("mèo") && message.content.toLowerCase().includes("mua")) {
             arrSend = ["Ông nói xem vì sao tôi lại phải trả lời ông? ", "Đi hỏi Robert Chen nhé ", "Mèo anh Chen, sang đấy mà hỏi "]
+        } else if (message.content.toLowerCase().includes("ey")) {
+            if (message.content.toLowerCase().includes("chưa")) {
+                arrSend = ["Chưa?", "Rồi", "Dĩ nhiên là chưa rồi <:chiu:588005258068885505>", "Hỏi ít thôi, ông khâu mồm mày lại đấy <:khinh:535335174427115521>"]
+            } else if (message.content.toLowerCase().includes("không")) {
+                arrSend = ["Không", "Đéo", "Dĩ nhiên là không <:chiu:588005258068885505>", "Rõ ràng là đéo rồi <:cuoi:535334346773757952>", "**Đéo** nhé <:phecan:537477148873588749>"]
+            } else if (message.content.toLowerCase().includes("à")) {
+                arrSend = ["Không", "Ờ", "Chuẩn", "Gì căng...."]
+            } else {
+                arrSend = ["Eyy", "Gì?"]
+            }
         } else {
             tagReturn(message)
             return
